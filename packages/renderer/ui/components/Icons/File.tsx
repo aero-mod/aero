27949b794823e7588aa531a9445e9eb1569c8943
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2023-07-09 20:50:18 +1000

    (ui/icons) Add File Icon

--- packages/renderer/ui/components/Icons/File.tsx
+++ packages/renderer/ui/components/Icons/File.tsx
@@ -16,17 +16,16 @@
  * along with Aero. If not, see <https://www.gnu.org/licenses/>.
  */
 
-export { default as External } from "./External";
-export { default as Sparkle } from "./Sparkle";
-export { default as Folder } from "./Folder";
-export { default as Check } from "./Check";
-export { default as Cross } from "./Cross";
-export { default as Cloud } from "./Cloud";
-export { default as File } from "./File";
-export { default as Copy } from "./Copy";
-export { default as Gear } from "./Gear";
-export { default as Play } from "./Play";
-export { default as Plus } from "./Plus";
-export { default as Book } from "./Book";
-export { default as Dev } from "./Dev";
-export { default as Eye } from "./Eye";
+import { IconProps, makeSvg } from "./base";
+
+export default (props: IconProps) => {
+    return makeSvg(
+        [
+            "M6,7 L2,7 L2,6 L6,6 L6,7 Z M8,5 L2,5 L2,4 L8,4 L8,5 Z M8,3 L2,3 L2,2 L8,2 L8,3 Z M8.88888889,0 L1.11111111,0 C0.494444444,0 0,0.494444444 0,1.11111111 L0,8.88888889 C0,9.50253861 0.497461389,10 1.11111111,10 L8.88888889,10 C9.50253861,10 10,9.50253861 10,8.88888889 L10,1.11111111 C10,0.494444444 9.5,0 8.88888889,0 Z",
+        ],
+        "0 0 10 10",
+        props.size || 16,
+        props.fill,
+        props.stroke
+    );
+};
